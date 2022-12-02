fdeef8f30f3bfc81f77f1a5c8e32c6b2638a219b
BangalaDur667 <Inismon.grossedaronnenoire@gmail.com>
2022-12-02 15:10:34 +0100

    qsqs

--- pages/index.js
+++ pages/index.js
@@ -143,9 +143,9 @@ const Header = () => {
           </span>
 
           <span className="text-4xl h-1/3 py-4 box-border font-bold flex justify-center w-full text-white z-30">
-            Vivre à{" "}
+            Vivre&nbsp;à&nbsp;{"  "}
             <a
-              className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+              className="underline hover:text-blue-200 hover:transition-all "
               href={
                 "https://www.google.fr/maps/place/Angers/@47.4819487,-0.5979541,13z/data=!3m1!4b1!4m5!3m4!1s0x480878da00e58e9d:0x40d37521e0d9c30!8m2!3d47.4711616!4d-0.5518257"
               }
@@ -316,7 +316,7 @@ export default function Home() {
                 "Angers est une ville en constante évolution, l'augmentation de la population de 25% en 30 ans illustre bien cette évolution, ainsi que toutes les nouvelles infrastructures de la ville en train d’émerger parmi lesquelles la 2nd ligne de tramway de la ville ainsi que la nouvelle patinoire "
               }
               <a
-                className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+                className="text-blue-400 underline hover:text-blue-500 hover:transition-all"
                 href={"https://www.angers-iceparc.com/"}
               >
                 Ice Parc
@@ -325,7 +325,7 @@ export default function Home() {
                 " qui a abrité la finale de coupe du monde de hockey sur glace féminin, dans des villes en constantes évolutions telles qu’Angers, les prix des habitations s’envolent souvent très vite, Angers n'échappe pas à la logique mais les prix restent raisonnables, ils sont actuellement de 3000€/ m² en moyenne.  Un grand quartier d’affaires est en construction dans le centre ville, ce qui pourrait encore élever le statut de la grande ville qu’est Angers. Depuis 2020, le groupe industriel énergétique français "
               }
               <a
-                className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+                className="text-blue-400 underline hover:text-blue-500 hover:transition-all"
                 href={"https://particuliers.engie.fr/"}
               >
                 Engie
@@ -334,7 +334,7 @@ export default function Home() {
                 " a décidé d'investir environ 120 millions d'euros en 25 ans pour faire de "
               }
               <a
-                className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+                className="text-blue-400 underline hover:text-blue-500 hover:transition-all"
                 href={
                   "https://www.engie.com/journalistes/communiques-de-presse/creer-premier-territoire-intelligent-france"
                 }
@@ -345,7 +345,7 @@ export default function Home() {
                 "de France, les innovations vont notamment viser l'optimisation de l'éclairage publique. La courbe d'évolution d'Angers est également propulsé par ses liens avec les autres métropoles avoisinantes facilités par la gare d'Angers,"
               }{" "}
               <a
-                className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+                className="text-blue-400 underline hover:text-blue-500 hover:transition-all"
                 href={
                   "https://www.garesetconnexions.sncf/fr/gares-services/angers-saint-laud"
                 }
@@ -395,7 +395,7 @@ export default function Home() {
                 "Angers fournit une atmosphère de vie des plus agréables tout d’abord de part sa verdure qui lui a permi d’accéder au rang de ville la plus verte de France 3 fois consécutives ("
               }
               <a
-                className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+                className="text-blue-400 underline hover:text-blue-500 hover:transition-all"
                 href={
                   "https://www.lefigaro.fr/flash-actu/2014/02/19/97001-20140219FILWWW00075-angers-ville-la-plus-verte-de-france.php"
                 }
@@ -404,7 +404,7 @@ export default function Home() {
               </a>
               {"-"}
               <a
-                className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+                className="text-blue-400 underline hover:text-blue-500 hover:transition-all"
                 href={
                   "https://www.lenergietoutcompris.fr/actualites-conseils/villes-les-plus-vertes-de-france-qui-est-en-tete-48117#:~:text=Comme%20lors%20du%20premier%20palmar%C3%A8s%20%C3%A9tabli%20en%202014%2C,professionnelle%20regroupant%20les%20responsables%20d%E2%80%99espaces%20verts%20en%20ville."
                 }
@@ -413,7 +413,7 @@ export default function Home() {
               </a>
               {"-"}
               <a
-                className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+                className="text-blue-400 underline hover:text-blue-500 hover:transition-all"
                 href={
                   "https://www.18h39.fr/articles/classement-villes-vertes-france.html"
                 }
@@ -434,7 +434,7 @@ export default function Home() {
                 ", où se développent de nombreuses plantes, exotiques ou non, qui nécessitent le climat tempéré d’Angers pour vivre. L’atmosphère de vie d’Angers est également favorisée par le faible trafic automobile qui est notamment dû à la forte densité de transports en commun au sein de la ville (Tramways, Bus, Navettes). Le faible trafic automobile associé au transports en communs et a la verdure fait de Angers "
               }
               <a
-                className="text-blue-300 underline hover:text-blue-400 hover:transition-all"
+                className="text-blue-400 underline hover:text-blue-500 hover:transition-all"
                 href={
                   "https://www.linternaute.com/actualite/monde/1031263-particules-fines-les-villes-les-plus-et-les-moins-polluees-du-monde/1031269-villes-les-moins-polluees-d-europe"
                 }
